52c78747272bdbfdc7a2ea4aeb75dcb80b9aae9e
claracampos <camposclr@gmail.com>
2020-01-13 14:20:15 -0400

    added logout button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { logout } from "../actions/authentication";
 
 export const Header = () => {
   return (
     <div>
       <h1>Travel Journal</h1>
+      <button onClick={logout}>Log Out</button>
     </div>
   );
 };
 
-export default Header
+export default Header;
