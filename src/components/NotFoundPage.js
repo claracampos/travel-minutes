ea72628e9e351af44d272e357ba7c29c4d33dc0c
claracampos <camposclr@gmail.com>
2020-01-13 13:32:54 -0400

    set up a draft 404 page

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFoundPage = () => (
+  <div>
+    <h1>404</h1>
+    <p>The page you're looking for could not be found.</p>
+  </div>
+);
+
+export default NotFoundPage;
