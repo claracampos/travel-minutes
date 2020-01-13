ea29e35970e0e8740d53e3092af886a014150dfd
claracampos <camposclr@gmail.com>
2020-01-13 11:52:09 -0400

    added draft Header and AddEntry components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./firebase/firebase";
+import Header from "./components/Header";
+import AddEntry from "./components/AddEntry";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <AddEntry />
+    </div>
+  );
 }
 
 export default App;
