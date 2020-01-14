4569b072cb0671ed1ea3b74076df842a0220c5c5
claracampos <camposclr@gmail.com>
2020-01-14 18:04:55 -0400

    moved data fetching to Dashboard component, set it up with useEffect to use async/await

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { database } from "../firebase/firebase";
+import AppContext from "../context/AppContext";
 
-const DashboardPage = () => (
-  <div>
-    <h1>Dashboard</h1>
-  </div>
-);
+const DashboardPage = () => {
+  const { user } = useContext(AppContext);
+  const [journalEntries, setJournalEntries] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await database.ref(`users/${user}`).once("value");
+      const entries = [];
+
+      data.forEach(entry => {
+        entries.push({ id: entry.key, ...entry.val() });
+      });
+      setJournalEntries(entries);
+    };
+
+    fetchData();
+  }, [user]);
+
+  console.log(journalEntries);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
 
 export default DashboardPage;
