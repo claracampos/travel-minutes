863ce9a7c7623e3490617e61209d6ae4ac9731ac
claracampos <camposclr@gmail.com>
2020-01-15 12:58:20 -0400

    added a redirect to dashboard (runs after addEntry is successful)

--- src/components/AddEntryForm.js
+++ src/components/AddEntryForm.js
@@ -2,15 +2,18 @@ import React, { useContext } from "react";
 import AppContext from "../context/AppContext";
 import TextInput from "./TextInput";
 import { addEntry } from "../actions/entryActions";
+import { useHistory } from "react-router-dom";
 
 const AddEntryForm = () => {
   const { user } = useContext(AppContext);
+  const history = useHistory();
+
   return (
     <div>
       <form
         id="add-entry-form"
         onSubmit={e => {
-          addEntry(e, user);
+          addEntry(e, user, history.push("/dashboard"));
         }}
       >
         <TextInput name="place" label="Place" />
