f95081a4b376809434d06876c8b256c2d970624c
claracampos <camposclr@gmail.com>
2020-01-15 14:04:44 -0400

    added redirect to dashboard once editing is complete

--- src/components/EditEntryForm.js
+++ src/components/EditEntryForm.js
@@ -2,17 +2,19 @@ import React, { useContext } from "react";
 import AppContext from "../context/AppContext";
 import TextInput from "./TextInput";
 import { editEntry } from "../actions/entryActions";
+import { useHistory } from "react-router-dom";
 
 const EditEntryForm = props => {
   const { user } = useContext(AppContext);
   const { place, seen, done, met, id } = props.entry;
+  const history = useHistory();
 
   return (
     <div>
       <form
         id="edit-entry-form"
         onSubmit={e => {
-          editEntry(e, user, id);
+          editEntry(e, user, id, history.push("/dashboard"));
         }}
       >
         <TextInput name="place" defaultValue={place} />
