c761a04304fd44eadaba7d33659b1d71081f0da8
claracampos <camposclr@gmail.com>
2020-01-15 16:58:08 -0400

    Added date state and date picker to form

--- src/components/AddEntryForm.js
+++ src/components/AddEntryForm.js
@@ -1,21 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AppContext from "../context/AppContext";
 import TextInput from "./TextInput";
 import { addEntry } from "../actions/entryActions";
 import { useHistory } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddEntryForm = () => {
   const { user } = useContext(AppContext);
   const history = useHistory();
+  const [date, setDate] = useState();
 
   return (
     <div>
       <form
         id="add-entry-form"
         onSubmit={e => {
-          addEntry(e, user, () => history.push("/dashboard"));
+          addEntry(e, user, date, () => history.push("/dashboard"));
         }}
       >
+        <label>Date: </label>
+        <DatePicker selected={date} onChange={setDate} required />
+
         <TextInput name="place" label="Place" />
         <TextInput name="seen" label="What I saw" />
         <TextInput name="done" label="What I did" />
