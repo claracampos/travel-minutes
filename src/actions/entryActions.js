027ed90bb280f70aa8a6ed35e0ed838abd6affa3
claracampos <camposclr@gmail.com>
2020-01-15 12:58:53 -0400

    added callbacks for redirects and errors

--- src/actions/entryActions.js
+++ src/actions/entryActions.js
@@ -1,25 +1,31 @@
 import { database } from "../firebase/firebase";
 
-const addEntry = (e, user) => {
+const addEntry = (e, user, callback) => {
   e.preventDefault();
 
-  database.ref(`users/${user}`).push({
-    place: e.target.elements.place.value,
-    seen: e.target.elements.seen.value,
-    done: e.target.elements.done.value,
-    met: e.target.elements.met.value
-  });
+  database.ref(`users/${user}`).push(
+    {
+      place: e.target.elements.place.value,
+      seen: e.target.elements.seen.value,
+      done: e.target.elements.done.value,
+      met: e.target.elements.met.value
+    },
+    error => (error ? alert(error) : callback)
+  );
 };
 
 const editEntry = (e, user, id) => {
   e.preventDefault();
 
-  database.ref(`users/${user}/${id}`).update({
-    place: e.target.elements.place.value,
-    seen: e.target.elements.seen.value,
-    done: e.target.elements.done.value,
-    met: e.target.elements.met.value
-  });
+  database.ref(`users/${user}/${id}`).set(
+    {
+      place: e.target.elements.place.value,
+      seen: e.target.elements.seen.value,
+      done: e.target.elements.done.value,
+      met: e.target.elements.met.value
+    },
+    error => (error ? alert(error) : window.location.reload(true))
+  );
 };
 
 export { addEntry, editEntry };
