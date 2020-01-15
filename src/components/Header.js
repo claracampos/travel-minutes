2ffb45cd1192bad2dc08183f89cfcf11fac1e5e1
claracampos <camposclr@gmail.com>
2020-01-15 17:14:23 -0400

    added draft nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { logout } from "../actions/authentication";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div>
       <h1>Travel Journal</h1>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/add-entry">Add Entry</Link>
+
       <button onClick={logout}>Log Out</button>
     </div>
   );
