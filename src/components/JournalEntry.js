c227e55194b2bc103b717386724bb2b21e4b2a71
claracampos <camposclr@gmail.com>
2020-01-15 09:33:06 -0400

    Fixed misnamed variable and added an "Edit" link to each entry

--- src/components/JournalEntry.js
+++ src/components/JournalEntry.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const JournalEntry = props => {
-  const { place, saw, done, people } = props.entry;
+  const { place, seen, done, people } = props.entry;
 
   return (
     <div>
       <h3>{place}</h3>
       <div>
-        <p>What I saw: {saw}</p>
+        <p>What I saw: {seen}</p>
         <p>What I did: {done}</p>
         <p>People I met: {people}</p>
+        <a href="/edit">Edit</a>
       </div>
     </div>
   );
