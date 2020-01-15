f8a51f11f6171a1cbe236726e170129cbc62f030
claracampos <camposclr@gmail.com>
2020-01-15 16:54:39 -0400

    incorporated date in addEntry( )

--- src/actions/entryActions.js
+++ src/actions/entryActions.js
@@ -1,10 +1,12 @@
 import { database } from "../firebase/firebase";
 
-const addEntry = (e, user, callback) => {
+const addEntry = (e, user, date, callback) => {
   e.preventDefault();
+  const formattedDate = date.toLocaleDateString();
 
   database.ref(`users/${user}`).push(
     {
+      date: formattedDate,
       place: e.target.elements.place.value,
       seen: e.target.elements.seen.value,
       done: e.target.elements.done.value,
@@ -17,7 +19,7 @@ const addEntry = (e, user, callback) => {
 const editEntry = (e, user, id, callback) => {
   e.preventDefault();
 
-  database.ref(`users/${user}/${id}`).set(
+  database.ref(`users/${user}/${id}`).update(
     {
       place: e.target.elements.place.value,
       seen: e.target.elements.seen.value,
