56b679282f8cdca22acb44ef03cd76fdb0beabb8
claracampos <camposclr@gmail.com>
2020-01-15 11:33:26 -0400

    changed db field from "people" to "met" for consistency, renamed component AddEntryForm

--- src/components/AddEntryForm.js
+++ src/components/AddEntryForm.js
@@ -1,25 +1,26 @@
 import React, { useContext } from "react";
 import AppContext from "../context/AppContext";
 import TextInput from "./TextInput";
+import { addEntry } from "../actions/entryActions";
 
-const EntryForm = props => {
+const AddEntryForm = () => {
   const { user } = useContext(AppContext);
   return (
     <div>
       <form
         id="add-entry-form"
         onSubmit={e => {
-          props.action(e, user);
+          addEntry(e, user);
         }}
       >
         <TextInput name="place" label="Place" />
         <TextInput name="seen" label="What I saw" />
         <TextInput name="done" label="What I did" />
-        <TextInput name="people" label="People I met" />
-        <button>{props.button}</button>
+        <TextInput name="met" label="People I met" />
+        <button>Add Entry</button>
       </form>
     </div>
   );
 };
 
-export default EntryForm;
+export default AddEntryForm;
--- src/components/AddEntryPage.js
+++ src/components/AddEntryPage.js
@@ -1,12 +1,11 @@
 import React from "react";
-import handleEntryFormSubmit from "../actions/addEntryScripts";
-import EntryForm from "./EntryForm";
+import AddEntryForm from "./AddEntryForm";
 
 const AddEntryPage = () => {
   return (
     <div>
       <h2>Add Entry</h2>
-      <EntryForm action={handleEntryFormSubmit} button={"Add Entry"} />
+      <AddEntryForm />
     </div>
   );
 };
