e0289d44f900ad4a864853c1f0438a32d2d196cf
claracampos <camposclr@gmail.com>
2020-01-16 13:44:08 -0400

    created individual search result component

---
+++ src/components/SearchResult.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SearchResult = props => {
+  const { id, date, place, seen, done, met } = props.entry;
+  return (
+    <div>
+      <p>
+        <b>
+          {date} - {place}
+        </b>{" "}
+        <Link to={`/edit-entry/${id}`}>Edit</Link>
+      </p>
+      <p>What I saw: {seen}</p>
+      <p>What I did: {done}</p>
+      <p>People I met: {met}</p>
+    </div>
+  );
+};
+
+export default SearchResult;
