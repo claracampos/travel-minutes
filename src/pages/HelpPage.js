81922a5717a6dc008d6200bfc7a1bb901fb0a013
claracampos <camposclr@gmail.com>
2020-01-17 15:54:11 -0400

    created help page

---
+++ src/pages/HelpPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PublicNavBar from "../components/PublicNavBar";
+
+const HelpPage = () => {
+  return (
+    <div>
+      <PublicNavBar />
+      <h1>Help</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
