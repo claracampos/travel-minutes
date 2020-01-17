0d1a3c1d95caa8edc1d82462026f82b7d77a5fd5
claracampos <camposclr@gmail.com>
2020-01-17 15:24:57 -0400

    cleaned up code

--- src/components/search/SearchView.js
+++ src/components/search/SearchView.js
@@ -1,32 +1,39 @@
 import React, { useState } from "react";
-import { searchEntries } from "../../utils/entryUtils";
 import SearchResultsList from "./SearchResultsList";
+import { searchEntries } from "../../utils/entryUtils";
 
 const SearchView = props => {
-  const entries = props.entries;
-  const setSearchView = props.action;
+  const { journalEntries, setSearchView } = props;
   const [searchResults, setSearchResults] = useState();
 
   return (
     <div>
       <h1>Dashboard</h1>
       <h2>Search</h2>
-      <button
-        onClick={() => {
-          setSearchView(false);
-        }}
-      >
-        Go back
-      </button>
-      <br></br>
-      <label>Search: </label>
-      <input
-        onChange={e => {
-          searchEntries(e, entries, setSearchResults);
-        }}
-      ></input>
-      {searchResults && <SearchResultsList results={searchResults} />}
-      {!searchResults && <p>Enter your search term above.</p>}
+      <div>
+        <button
+          onClick={() => {
+            setSearchView(false);
+          }}
+        >
+          Go back
+        </button>
+      </div>
+      <div>
+        <label>Search: </label>
+        <input
+          onChange={e => {
+            searchEntries(e, journalEntries, setSearchResults);
+          }}
+        ></input>
+      </div>
+      <div>
+        {searchResults ? (
+          <SearchResultsList results={searchResults} />
+        ) : (
+          <p>Enter your search term above.</p>
+        )}
+      </div>
     </div>
   );
 };
