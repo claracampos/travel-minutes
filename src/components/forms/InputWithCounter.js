b5aaa5e296da3aea116fc5e2b11f7acc6f0ea02f
claracampos <camposclr@gmail.com>
2020-01-17 15:35:09 -0400

    cleaned up code

--- src/components/forms/InputWithCounter.js
+++ src/components/forms/InputWithCounter.js
@@ -1,22 +1,20 @@
 import React from "react";
-import CharacterCount from "./CharacterCount";
+import CharacterCounter from "./CharacterCounter";
 
-const InputWithCounter = props => {
-  return (
-    <div>
-      {props.label && <label>{props.label}: </label>}
-      <input
-        type="text"
-        name={props.name}
-        defaultValue={props.defaultValue || ""}
-        required
-        maxLength={100}
-        autoComplete="off"
-        onChange={e => props.setCount(e.target.value.length)}
-      ></input>
-      <CharacterCount count={props.count} />
-    </div>
-  );
-};
+const InputWithCounter = props => (
+  <div>
+    <label>{props.label}: </label>
+    <input
+      type="text"
+      name={props.name}
+      defaultValue={props.defaultValue || ""}
+      maxLength="100"
+      autoComplete="off"
+      required
+      onChange={e => props.setCount(e.target.value.length)}
+    ></input>
+    <CharacterCounter count={props.count} />
+  </div>
+);
 
 export default InputWithCounter;
