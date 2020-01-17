f03017674d59d5f51229060ccfa6ab56a2b0b843
claracampos <camposclr@gmail.com>
2020-01-17 15:35:25 -0400

    renamed file/component

--- src/components/forms/CharacterCounter.js
+++ src/components/forms/CharacterCounter.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const CharacterCount = props => {
+const CharacterCounter = props => {
   const count = props.count;
   return count ? <p>{count}/100</p> : <p>0/100</p>;
 };
 
-export default CharacterCount;
+export default CharacterCounter;
