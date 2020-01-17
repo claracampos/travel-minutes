43014ab18b729a20a142927425d4f7d44e869144
claracampos <camposclr@gmail.com>
2020-01-17 13:03:49 -0400

    readability adjustments

--- src/components/forms/AddEntryForm.js
+++ src/components/forms/AddEntryForm.js
@@ -1,50 +1,58 @@
 import React, { useContext, useState } from "react";
-import AppContext from "../../context/AppContext";
-import InputWithCounter from "./InputWithCounter";
-import { addEntry } from "../../utils/entryUtils";
 import { useHistory } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import AppContext from "../../context/AppContext";
+import InputWithCounter from "./InputWithCounter";
+import { addEntry } from "../../utils/entryUtils";
 
 const AddEntryForm = () => {
   const { user } = useContext(AppContext);
   const history = useHistory();
+  const [seenInputCount, setSeenInputCount] = useState(0);
+  const [doneInputCount, setDoneInputCount] = useState(0);
+  const [metInputCount, setMetInputCount] = useState(0);
   const [date, setDate] = useState();
-  const [seenCount, setSeenCount] = useState(0);
-  const [doneCount, setDoneCount] = useState(0);
-  const [metCount, setMetCount] = useState(0);
+
   return (
     <div>
       <form
-        id="add-entry-form"
         onSubmit={e => {
           addEntry(e, user, date, () => history.push("/dashboard"));
         }}
       >
-        <label>Date: </label>
-        <DatePicker selected={date} onChange={setDate} required />
-
-        <label>Place: </label>
-        <input name="place" maxLength={50} required autoComplete="off" />
-        <InputWithCounter
-          name="seen"
-          label="What I saw"
-          setCount={setSeenCount}
-          count={seenCount}
-        />
-
-        <InputWithCounter
-          name="done"
-          label="What I did"
-          setCount={setDoneCount}
-          count={doneCount}
-        />
-        <InputWithCounter
-          name="met"
-          label="People I met"
-          setCount={setMetCount}
-          count={metCount}
-        />
+        <div>
+          <label>Date: </label>
+          <DatePicker selected={date} onChange={setDate} required />
+        </div>
+        <div>
+          <label>Place: </label>
+          <input name="place" maxLength={50} required autoComplete="off" />
+        </div>
+        <div>
+          <InputWithCounter
+            name="seen"
+            label="What I saw"
+            setCount={setSeenInputCount}
+            count={seenInputCount}
+          />
+        </div>
+        <div>
+          <InputWithCounter
+            name="done"
+            label="What I did"
+            setCount={setDoneInputCount}
+            count={doneInputCount}
+          />
+        </div>
+        <div>
+          <InputWithCounter
+            name="met"
+            label="People I met"
+            setCount={setMetInputCount}
+            count={metInputCount}
+          />
+        </div>
         <button>Add Entry</button>
       </form>
     </div>
