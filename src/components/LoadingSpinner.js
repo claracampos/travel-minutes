6cdaf8ce99f3e124f17458a4b2d3370b2d1a5fad
claracampos <camposclr@gmail.com>
2020-01-18 13:57:24 -0400

    created loading spinner component

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingSpinner = () => (
+  <div class="text-center">
+    <div class="spinner-border text-secondary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+);
+
+export default LoadingSpinner;
