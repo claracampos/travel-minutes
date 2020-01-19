e3d0c3092e8bcca4b062b70f8f3333a3fe8abd21
claracampos <camposclr@gmail.com>
2020-01-19 18:29:06 -0400

    styled component

--- src/components/entries/JournalEntry.js
+++ src/components/entries/JournalEntry.js
@@ -4,26 +4,36 @@ import { Link } from "react-router-dom";
 const JournalEntry = props => {
   const { id, date, place, seen, done, met } = props.entry;
   const [fullEntry, setFullEntry] = useState(false);
-  const buttonText = fullEntry ? "Hide" : "Show";
+  const buttonText = fullEntry ? "-" : "+";
 
   return (
-    <div>
-      <h3>
-        {date} - {place}
-      </h3>
-      <button
-        onClick={() => {
-          setFullEntry(!fullEntry);
-        }}
-      >
-        {buttonText}
-      </button>
+    <div class="container-fluid d-flex flex-column border rounded shadow-sm my-2">
+      <div class="d-flex flex-row justify-content-between align-items-center">
+        <p class="text-wrap lead text-break w-75 pt-2">
+          {date} - <span class="h5 serif">{place}</span>
+        </p>
+        <button
+          type="button"
+          class="btn btn-sm btn-rounded btn-outline-secondary tiny-button p-0"
+          onClick={() => {
+            setFullEntry(!fullEntry);
+          }}
+        >
+          {buttonText}
+        </button>
+      </div>
       {fullEntry && (
-        <div>
-          <p>What I saw: {seen}</p>
-          <p>What I did: {done}</p>
-          <p>People I met: {met}</p>
-          <Link to={`/edit-entry/${id}`}>Edit</Link>
+        <div class="mb-2">
+          <p class="mb-1">
+            <b>What I saw:</b> {seen}
+          </p>
+          <p class="mb-1">
+            <b>What I did:</b> {done}
+          </p>
+          <p class="mb-3">
+            <b>People I met:</b> {met}
+          </p>
+          <Link to={`/edit-entry/${id}`}>Edit entry</Link>
         </div>
       )}
     </div>
