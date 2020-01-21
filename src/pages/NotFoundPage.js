faf64b905eaf61f3c6034420270d1f8e02ef6b17
claracampos <camposclr@gmail.com>
2020-01-21 11:55:04 -0400

    styled component

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import AppContext from "../context/AppContext";
+import PrivateNavBar from "../components/PrivateNavBar";
+import PublicNavBar from "../components/PublicNavBar";
 import GoHomeButton from "../components/GoHomeButton";
 
-const NotFoundPage = () => (
-  <div>
-    <h1>404</h1>
-    <p>The page you're looking for could not be found.</p>
-    <GoHomeButton>Go Home</GoHomeButton>
-  </div>
-);
-
+const NotFoundPage = () => {
+  const { user } = useContext(AppContext);
+  return (
+    <div>
+      {user ? <PrivateNavBar /> : <PublicNavBar />}
+      <div className="m-3">
+        <h1 className="serif text-left pb-3">404 - Page Not Found</h1>
+        <p className="lead mb-5">The page you requested does not exist.</p>
+        <GoHomeButton>Go Home</GoHomeButton>
+      </div>
+    </div>
+  );
+};
 export default NotFoundPage;
