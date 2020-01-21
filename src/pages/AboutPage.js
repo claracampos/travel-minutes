c6631a591f3a9e8b4498795dafa10ffa5ac3efe1
claracampos <camposclr@gmail.com>
2020-01-21 11:56:06 -0400

    added content and style

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
+import { a } from "react-router-dom";
+import AppContext from "../context/AppContext";
+import PrivateNavBar from "../components/PrivateNavBar";
+import PublicNavBar from "../components/PublicNavBar";
+import photo from "../media/photo.jpg";
 
 const AboutPage = () => {
+  const { user } = useContext(AppContext);
   return (
     <div>
-      <h1>About</h1>
+      {user ? <PrivateNavBar /> : <PublicNavBar />}
+      <div className="m-3">
+        <h1 className="serif text-left pb-3">About</h1>
+        <p className="text-center">
+          <i>Travel Minutes</i> was developed by:
+        </p>
+        <div className="d-flex flex-column align-items-center mb-1">
+          <img
+            className="rounded-circle photo"
+            src={photo}
+            alt="Clara Campos"
+          />
+          <p className="font-weight-bold text-secondary m-0">Clara Campos</p>
+
+          <p className="font-weight-light text-secondary m-0">
+            Prince Edward Island, Canada
+          </p>
+          <p>
+            <a
+              href="https://github.com/claracampos"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mr-1"
+            >
+              GitHub
+            </a>
+            |
+            <a
+              href="https://claralearnstocode.wordpress.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-1"
+            >
+              Blog
+            </a>
+          </p>
+        </div>
+        <p className="text-justify">
+          This application was built on React and connected to Firebase for
+          authentication and data storage. It was tested using Jest and visually
+          designed with Bootstrap. You can browse the code here.
+        </p>
+      </div>
     </div>
   );
 };
