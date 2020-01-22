87c33ae7b38da8004e482658c20574937ab6ef0a
claracampos <camposclr@gmail.com>
2020-01-22 10:56:11 -0400

    added container style (for larger screens)

--- src/components/search/SearchView.js
+++ src/components/search/SearchView.js
@@ -8,36 +8,38 @@ const SearchView = props => {
   const [searchResults, setSearchResults] = useState();
 
   return (
-    <div>
-      <PrivateNavBar />
-      <div className="m-3">
-        <h1 className="serif text-left">My Travel Log</h1>
-        <div>
-          <button
-            onClick={() => {
-              setSearchView(false);
-            }}
-            className="btn btn-sm btn-primary rounded-pill mb-2"
-          >
-            Go back
-          </button>
-        </div>
-        <div>
-          <div className="d-flex flex-row bg-light border rounded p-2">
-            <label className="lead mr-2">Search: </label>
-            <input
-              onChange={e => {
-                searchEntries(e, journalEntries, setSearchResults);
+    <div className="d-flex justify-content-center bg-light">
+      <div className="main-container shadow-lg">
+        <PrivateNavBar />
+        <div className="p-4">
+          <h1 className="serif text-left">My Travel Log</h1>
+          <div>
+            <button
+              onClick={() => {
+                setSearchView(false);
               }}
-              className="form-control flex-grow-1"
-            ></input>
+              className="btn btn-sm btn-primary rounded-pill mb-2"
+            >
+              Go back
+            </button>
           </div>
-          <div className="p-1">
-            {searchResults ? (
-              <SearchResultsList results={searchResults} />
-            ) : (
-              <p>Enter your search term above.</p>
-            )}
+          <div>
+            <div className="d-flex flex-row bg-light border rounded p-2">
+              <label className="lead mr-2">Search: </label>
+              <input
+                onChange={e => {
+                  searchEntries(e, journalEntries, setSearchResults);
+                }}
+                className="form-control flex-grow-1"
+              ></input>
+            </div>
+            <div className="p-1">
+              {searchResults ? (
+                <SearchResultsList results={searchResults} />
+              ) : (
+                <p>Enter your search term above.</p>
+              )}
+            </div>
           </div>
         </div>
       </div>
